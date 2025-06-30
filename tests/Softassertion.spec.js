93da9b08f5afcecf6a738e32830ddf9319c5fd06
TiwariMithilesh <your-new-email@example.com>
2025-06-30 14:11:17 +0530

    Fix some issue

--- tests/Softassertion.spec.js
+++ tests/Softassertion.spec.js
@@ -12,7 +12,7 @@ test("Soft Assertion", async ({page})=>{
 
     //Soft Assertion
        //Hard Assertion
-    await expect.soft(page).toHaveTitle('STORE1234')// This assertion will fail but the test will continue 
+    await expect.soft(page).toHaveTitle('STORE')// This assertion will fail but the test will continue 
     await expect.soft(page).toHaveURL('https://www.demoblaze.com/index.html')
     await expect.soft(page.locator('.navbar-brand')).toBeVisible()
 
